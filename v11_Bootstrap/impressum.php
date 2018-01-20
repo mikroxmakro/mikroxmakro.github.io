ad802a2abe07470eebef8f2e207375acaa8488ce
mikroxmakro <info@mikromalmakro.de>
2018-01-20 18:56:25 +0000

    Bewertungssiegel und kleine Fehlerbehebungen

---
+++ v11_Bootstrap/impressum.php
@@ -0,0 +1,22 @@
+    <div class="container-fluid py-5" id="big_wrapper">
+        <div class="row">
+ 		<div class="col-sm-8 pb-3">
+ 		<section>
+ 		<h2>Impressum</h2>
+        <h6>Angaben gemäß § 5 TMG:</h6>
+        Mikromalmakro<br />
+        Patrick Brockmann<br />
+		Aschaffenburger Str. 19<br />
+        63073 Offenbach am Main<br />
+        <br />
+		Telefon:	01777303300<br />
+		E-Mail:	<a href="mailto: patrickbrockmann@mikromalmakro.de" target="_top">patrickbrockmann@mikromalmakro.de</a>
+        </section>
+        </div>
+        <div class="col-sm-4">
+		    <!-- ProvenExpert Bewertungssiegel -->
+<a  href="https://www.provenexpert.com/mikromalmakro/?utm_source=Widget&amp;utm_medium=Widget&amp;utm_campaign=Widget" title="Erfahrungen &amp; Bewertungen zu Mikromalmakro anzeigen" target="_blank" style="text-decoration:none;"><img src="https://images.provenexpert.com/55/d8/4819cadade7b816d1fc09ec84cab/widget_landscape_200_de_0.png" alt="Erfahrungen &amp; Bewertungen zu Mikromalmakro" width="200" height="167" style="border:0" /></a>
+<!-- ProvenExpert Bewertungssiegel -->
+		</div>
+		</div>
+    </div>
---
+++ v11_Bootstrap/kontaktseite.php
@@ -0,0 +1,24 @@
+<div class="container">
+<div class="row">
+<div class="col-md-7"></div>
+<div class="col-md-5">
+    <form method="post" action="formular/form-mailer.php">
+        <div class="form-group">
+        <label for="Name"><b>Name:</b></label><br>
+        <input class="form-control" type="text" id="Name" name="Name"><br>
+         
+        <label for="Email"><b>E-Mail:</b></label><br>
+        <input class="form-control" type="text" id="Email" name="Email"><br>
+         
+        <label for="Betreff"><b>Betreff:</b></label><br>
+        <input class="form-control" type="text" id="Betreff" name="Betreff"><br>
+         
+        <label for="Nachricht"><b>Nachricht:</b></label><br>
+        <textarea class="form-control" id="Nachricht" name="Nachricht" rows="10" cols="50"></textarea><br>
+         
+        <input class="form-control" type="submit" name="submit">
+        </div>
+    </form>
+</div>
+</div>
+</div>
